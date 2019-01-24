2c8c8b8f573231f63aade01c5bcb873b163c11e1
Max Stern <stern.maxwell@gmail.com>
2019-01-24 17:13:41 -0600

    react router setup

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home/home';
+
+class Routes extends Component {
+    render() {
+        return(
+            <Switch>
+                <Route path="/" exact component={Home}/>
+            </Switch>
+        )
+    }
+}
+
+export default Routes;
