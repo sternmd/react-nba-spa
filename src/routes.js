2df8ef3368885379affecfc0e0d9c4b180940574
Max Stern <stern.maxwell@gmail.com>
2019-01-24 18:47:13 -0600

    layout

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
+import Layout from './components/hoc/Layout/layout';
 
 class Routes extends Component {
     render() {
         return(
-            <Switch>
-                <Route path="/" exact component={Home}/>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                </Switch>
+            </Layout>
         )
     }
 }
