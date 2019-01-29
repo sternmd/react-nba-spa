7527e59b3dabccadfa6fcb63cea1af08995d213d
Max Stern <stern.maxwell@gmail.com>
2019-01-29 13:17:18 -0600

    footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './footer.css';
 
 const Footer = () => {
+
+    const CURRENT_YEAR = new Date().getFullYear();
+
     return(
-        <header>
-            FOOTER
-        </header>
+        <div>
+            <div className="footer">
+                <Link to="/" className="logo"><img alt="logo" src="/images/nba_logo.png" /></Link>
+            <div className="footer-right">
+                @NBA {CURRENT_YEAR} All rights reserved. 
+            </div>
+            </div>
+        </div>
     )
 }
 
