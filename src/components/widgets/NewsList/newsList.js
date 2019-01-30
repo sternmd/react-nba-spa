f952ae109e6a35dba496f861422f90e982b8ac80
Max Stern <stern.maxwell@gmail.com>
2019-01-30 16:41:40 -0600

    load more

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -3,6 +3,8 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import './newsList.css';
+
 import { URL } from '../../../config';
 
 class NewsList extends Component {
@@ -14,7 +16,11 @@ class NewsList extends Component {
     }
 
     componentWillMount(){
-        axios.get(`${URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
+        this.request(this.state.start, this.state.end);
+    }
+
+    request = (start,end) => {
+        axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
         .then(response => {
             this.setState({
                 items: [...this.state.items, ...response.data]
@@ -22,11 +28,40 @@ class NewsList extends Component {
         })
     }
 
+    loadMore = () => {
+        let end = this.state.end + this.state.amount;
+        this.request(this.state.end, end)
+    }
+
+    renderNews = (type) => {
+        let template = null;
+
+        switch(type){
+            case('card'):
+            template= this.state.items.map((item,i) => {
+                return (
+                    <div className="newsList">
+                        <div className="newsList_item">
+                            <Link to={`/articles/${item.id}`}>
+                            <h2>{item.title}</h2>
+                            </Link>
+                        </div>
+                    </div>
+                )
+            })
+            break;
+        default: 
+            template = null;
+        }
+        return template;
+    }
+
     render() {
         console.log(this.state.items)
         return (
             <div>
-                NEWS
+                {this.renderNews(this.props.type)}
+                <div onClick={()=> this.loadMore()}>LOAD MORE</div>
             </div>
         );
     }
