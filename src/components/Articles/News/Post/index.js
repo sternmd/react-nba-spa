d9659cd4ef47b61ba0d562cd90e7e8bfa5b3e0ed
Max Stern <stern.maxwell@gmail.com>
2019-02-20 18:13:16 -0600

    article body

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -4,7 +4,6 @@ import { URL } from '../../../../config';
 
 import '../../articles.css';
 import Header from './header';
-import Body from './body';
 
 class NewsArticles extends Component {
 
@@ -39,7 +38,17 @@ class NewsArticles extends Component {
                     date={article.date}
                     author={article.author}
                 />
-                <Body/>
+                <div className="articleBody">
+                    <h1>{article.title}</h1>
+                    <div className="articleImage"
+                    style={{
+                        background: `url('/images/articles/${article.image}')`
+                    }}
+                    ></div>
+                    <div className="articleText">
+                        {article.body}
+                    </div>
+                </div>
             </div>
         );
     }
