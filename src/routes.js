4c6b67eacaea8f709b488cf6f7c968efec36b513
Max Stern <stern.maxwell@gmail.com>
2019-02-22 17:24:05 -0600

    routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
 import Layout from './components/hoc/Layout/layout';
+import NewsMain from './components/Articles/News/Main/index'
 
 import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
@@ -14,6 +15,7 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/>
+                    <Route path="/news" exact component={NewsMain}/>
                     <Route path="/articles/:id" exact component={ NewsArticle } />
                     <Route path="/videos/:id" exact component={ VideoArticle } />
                 </Switch>
